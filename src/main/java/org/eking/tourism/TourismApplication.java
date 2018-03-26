d2c8619adb7d0bc17cc89c16af3cb14cff1792b0
王 宇 <wang.y14@haihangyun.com>
2018-03-26 22:35:22 +0800

    【增加微商城基本设置、广告图轮播、自定义功能图标等接口】

--- src/main/java/org/eking/tourism/TourismApplication.java
+++ src/main/java/org/eking/tourism/TourismApplication.java
@@ -5,12 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
-@Service
 public class TourismApplication {
 
 	public static void main(String[] args) {
-		System.out.println(123);
-        System.err.println(4);
 		SpringApplication.run(TourismApplication.class, args);
 	}
 }
